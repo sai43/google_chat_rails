ac5ee8952c52db2fbf5ae7bffe7bcf8269a93bb6
root <sai43>
2014-12-03 17:08:51 +0530

    inital commit

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# create default test user
+User.create!(name: "Sai", email: "chsai.btech@gmail.com", password: "password12", password_confirmation: "password12")
+
+# create default test user
+User.create!(name: "rails", email: "chsai.swe@gmail.com", password: "password12", password_confirmation: "password12")
